3fcf0228f20a1394bcce835a1abd16884a9d0877
William Takeshi Pereira <williamtpereira@gmail.com>
2019-12-10 18:36:06 -0300

    add day 03

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,64 @@
 defmodule AdventOfCode.Day03 do
   def part1(args) do
+    walk_description =
+      args
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, ","))
+
+    fst_path = walk_direction([{0, 0}], Enum.at(walk_description, 0))
+    snd_path = walk_direction([{0, 0}], Enum.at(walk_description, 1))
+
+    intersect_lst(fst_path, snd_path)
+    |> Enum.map(fn x -> manhattan_dist(x, {0, 0}) end)
+    |> Enum.min()
   end
 
   def part2(args) do
+    walk_description =
+      args
+      |> String.split("\n")
+      |> Enum.map(&String.split(&1, ","))
+
+    fst_path = walk_direction([{0, 0}], Enum.at(walk_description, 0))
+    snd_path = walk_direction([{0, 0}], Enum.at(walk_description, 1))
+
+    intersect_lst(fst_path, snd_path)
+    |> Enum.map(fn x -> path_dist(x, fst_path, snd_path) end)
+    |> Enum.min()
+  end
+
+  def intersect_lst(lst1, lst2) do
+    lst3 = lst1 -- lst2
+    final_lst = lst1 -- lst3
+    final_lst -- [{0, 0}]
+  end
+
+  def manhattan_dist({x1, y1}, {x0, y0}) do
+    abs(x1 - x0) + abs(y1 - y0)
+  end
+
+  def path_dist(intersect_point, fst_path, snd_path) do
+    fst_path_distance = Enum.find_index(fst_path, fn x -> x == intersect_point end)
+    snd_path_distance = Enum.find_index(snd_path, fn x -> x == intersect_point end)
+
+    fst_path_distance + snd_path_distance
+  end
+
+  def walk_direction(coordinate_lst, [next_direction | tail]) do
+    new_coordinates = walk_wire(List.first(coordinate_lst), next_direction)
+    walk_direction(new_coordinates ++ coordinate_lst, tail)
+  end
+
+  def walk_direction(coordinate_lst, []) do
+    Enum.reverse(coordinate_lst)
+  end
+
+  def walk_wire({init_x, init_y}, direction) do
+    case direction do
+      "R" <> num -> for x <- String.to_integer(num)..1, do: {init_x + x, init_y}
+      "L" <> num -> for x <- String.to_integer(num)..1, do: {init_x - x, init_y}
+      "U" <> num -> for y <- String.to_integer(num)..1, do: {init_x, init_y + y}
+      "D" <> num -> for y <- String.to_integer(num)..1, do: {init_x, init_y - y}
+    end
   end
 end
--- test/advent_of_code/day_03_test.exs
+++ test/advent_of_code/day_03_test.exs
@@ -3,19 +3,53 @@ defmodule AdventOfCode.Day03Test do
 
   import AdventOfCode.Day03
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  # @tag :skip
+  test "test1part1" do
+    input = "R8,U5,L5,D3
+U7,R6,D4,L4"
     result = part1(input)
 
-    assert result
+    assert result == 6
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
+  test "test2part1" do
+    input = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83"
+    result = part1(input)
+
+    assert result == 159
+  end
+
+  test "test3part1" do
+    input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    result = part1(input)
+
+    assert result == 135
+  end
+
+  # @tag :skip
+  test "test1part2" do
+    input = "R8,U5,L5,D3
+U7,R6,D4,L4"
     result = part2(input)
 
-    assert result
+    assert result == 30
+  end
+
+  test "test2part2" do
+    input = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83"
+    result = part2(input)
+
+    assert result == 610
+  end
+
+  test "test3part2" do
+    input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    result = part2(input)
+
+    assert result == 410
   end
 end
