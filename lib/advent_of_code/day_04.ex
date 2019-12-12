9dffa120199a04ed1f5c2e6948de0731797fc623
William Takeshi Pereira <williamtpereira@gmail.com>
2019-12-12 16:00:39 -0300

    add day_04

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,53 @@
 defmodule AdventOfCode.Day04 do
+  def get_ranges(args) do
+    String.split(args, "-")
+    |> Enum.map(&String.to_integer/1)
+  end
+
   def part1(args) do
+    [range_init, range_finish] = get_ranges(args)
+
+    range_init..range_finish
+    |> Enum.filter(&check_passwd/1)
+    |> Enum.count()
   end
 
   def part2(args) do
+    [range_init, range_finish] = get_ranges(args)
+
+    range_init..range_finish
+    |> Enum.filter(&check_passwd_strict/1)
+    |> Enum.count()
+  end
+
+  def check_passwd(passwd) do
+    int_lst = Integer.digits(passwd)
+
+    adj_tuple = Enum.zip(int_lst, tl(int_lst))
+
+    have_pair(adj_tuple) && only_increase(adj_tuple)
+  end
+
+  def check_passwd_strict(passwd) do
+    int_lst = Integer.digits(passwd)
+
+    adj_tuple = Enum.zip(int_lst, tl(int_lst))
+
+    have_strict_pair(adj_tuple) && only_increase(adj_tuple)
+  end
+
+  defp have_pair(adj_tuple) do
+    Enum.any?(adj_tuple, fn {fst, snd} -> fst == snd end)
+  end
+
+  defp have_strict_pair(adj_tuple) do
+    Enum.any?(adj_tuple, fn {fst, snd} ->
+      fst == snd &&
+        Enum.count(adj_tuple) - (Enum.filter(adj_tuple, &(&1 != {fst, snd})) |> Enum.count()) == 1
+    end)
+  end
+
+  defp only_increase(adj_tuple) do
+    Enum.all?(adj_tuple, fn {fst, snd} -> fst <= snd end)
   end
 end
--- test/advent_of_code/day_04_test.exs
+++ test/advent_of_code/day_04_test.exs
@@ -3,6 +3,34 @@ defmodule AdventOfCode.Day04Test do
 
   import AdventOfCode.Day04
 
+  test "password1" do
+    input = 111_111
+    result = check_passwd(input)
+    assert result == true
+
+    input = 223_450
+    result = check_passwd(input)
+    assert result == false
+
+    input = 123_789
+    result = check_passwd(input)
+    assert result == false
+  end
+
+  test "password2" do
+    input = 112_233
+    result = check_passwd_strict(input)
+    assert result == true
+
+    input = 123_444
+    result = check_passwd_strict(input)
+    assert result == false
+
+    input = 111_122
+    result = check_passwd_strict(input)
+    assert result == true
+  end
+
   @tag :skip
   test "part1" do
     input = nil
